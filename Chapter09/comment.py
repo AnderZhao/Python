25d0c3996362b65b25faedccc4e8339e92a03eff
AnderZhao <Ander.Zhao@outlook.com>
2021-08-16 15:40:42 +0800

    Finish Chapter09

---
+++ Chapter09/comment.py
@@ -0,0 +1,14 @@
+# This is a comment in a Python program
+print 'This is not a comment'   # Calculate the area of the rectangle
+
+# ***************
+# This is a program to illustrate how comments are used in Python
+# The row of stars is used to visually separate the comments
+# from the rest of the code
+# ***************
+
+""" Here is a comment that is on multiple
+lines, using a triple-quoted string.
+It's not really a comment, but it
+behaves like one.
+"""
