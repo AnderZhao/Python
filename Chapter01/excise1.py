edf6626b3bc6c38fb24ea7e02b4c9e905a1a02ba
AnderZhao <Ander.Zhao@outlook.com>
2021-08-16 14:29:15 +0800

    Finish Chapter01

---
+++ Chapter01/excise1.py
@@ -0,0 +1,5 @@
+print "Calculate the number of minutes in a week"
+print "A week is 7 days."
+print "A day is 24 hours."
+print "A hour is 60 minutes."
+print "So the number of minutes in a week: 7 * 24 * 60 =", 7 * 24 * 60
---
+++ Chapter01/excise2.py
@@ -0,0 +1,3 @@
+print "My name is Ander Zhao."
+print "I was born February 22, 1999."
+print "My favorite color is green"
