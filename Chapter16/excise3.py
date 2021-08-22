9f520decef28f00ee3d4f6d6200277aa8b271b49
AnderZhao <Ander.Zhao@outlook.com>
2021-08-22 17:44:39 +0800

    Finish Chapter16

---
+++ Chapter16/excise3.py
@@ -0,0 +1,28 @@
+import pygame
+pygame.init()
+screen = pygame.display.set_mode([640, 480])
+screen.fill([255, 255, 255])
+my_ball = pygame.image.load('./img/beach_ball.png')
+x = 50
+y = 50
+x_speed = 10
+y_speed = 10
+time = 20
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    pygame.time.delay(time)
+    pygame.draw.rect(screen, [255, 255, 255], [x, y, 90, 90], 0)
+    x = x + x_speed
+    y = y + y_speed
+    if x > screen.get_width() - 90 or x < 0:
+        x_speed = - x_speed
+        time = - x_speed
+    if y > screen.get_height() - 90 or y < 0:
+        y_speed = - y_speed
+        time = + y_speed
+    screen.blit(my_ball, [x, y])
+    pygame.display.flip()
+pygame.quit()
